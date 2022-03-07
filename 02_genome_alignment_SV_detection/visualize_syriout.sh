8090a420119cbc419534b76462970c28d02ef5ce
kaede0e <91504464+kaede0e@users.noreply.github.com>
2022-03-07 10:36:45 -0800

    Update visualize_syriout.sh

--- 02_genome_alignment_SV_detection/visualize_syriout.sh
+++ 02_genome_alignment_SV_detection/visualize_syriout.sh
@@ -4,4 +4,4 @@
 PATH_TO_PLOTSR="/home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/syri/syri/bin/plotsr"
 
 # Visualize the alignment in PDF format (built-in SyRI function)
-python3 $PATH_TO_PLOTSR syri.out refgenome_chr.fa  -H 8 -W 5
+python3 $PATH_TO_PLOTSR syri.out refgenome_chr.fa qrygenome_chr.fa -H 8 -W 5
