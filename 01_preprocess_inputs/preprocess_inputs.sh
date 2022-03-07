6325f5e4326d4d9a7851e130327dfad79941b36a
kaede0e <91504464+kaede0e@users.noreply.github.com>
2022-03-06 18:49:30 -0800

    Update preprocess_inputs.sh

--- 01_preprocess_inputs/preprocess_inputs.sh
+++ 01_preprocess_inputs/preprocess_inputs.sh
@@ -18,7 +18,7 @@ cat refgenome.fa | grep ">CM" | cut -f 1 -d " " | sed s/">"//g > ref_chr_names.t
 ##column 1: name of chromosome
 ##column 2: start of chromosome
 ##column 3: end of chromosome    no header.
-#2.1) Cut only CDS rows 
+#2.1) Cut only CDS rows from the original gene annotation file
 awk '$3 ~ /CDS/' genome.gff > genome_cds.gff
-#2.2) Convert gff3 into bed
+#2.2) Convert gff3 into bed format
 gff2bed < genome_cds.gff > genome_cds.bed
