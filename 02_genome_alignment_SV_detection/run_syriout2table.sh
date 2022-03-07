2916b5730b210daf24969ae5f3982aaf8de4ed39
kaede0e <91504464+kaede0e@users.noreply.github.com>
2022-03-06 21:32:54 -0800

    Create run_syriout2table.sh

---
+++ 02_genome_alignment_SV_detection/run_syriout2table.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#7) Format SyRI output into tidy table
+cat invOut.txt | perl /PATH/TO/syriout2table.pl > invOut_table.txt
+cat synOut.txt | perl /PATH/TO/syriout2table.pl > synOut_table.txt
+cat TLOut.txt | perl /PATH/TO/syriout2table.pl > TLOut_table.txt
+cat invTLOut.txt | perl /PATH/TO/syriout2table.pl > invTLOut_table.txt
+cat invDupOut.txt | perl /PATH/TO/syriout2table.pl > invDupOut_table.txt
+cat dupOut.txt | perl /PATH/TO/syriout2table.pl > dupOut_table.txt
