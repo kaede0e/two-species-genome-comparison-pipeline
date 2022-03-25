077b014a0f661d56d3faa9111a1091e25f402a74
kaede0e <91504464+kaede0e@users.noreply.github.com>
2022-03-25 08:37:34 -0700

    Update EDTA_loop_for_chr_by_chr.sh

--- 04_de_novo_TE_annotation/EDTA_loop_for_chr_by_chr.sh
+++ 04_de_novo_TE_annotation/EDTA_loop_for_chr_by_chr.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #!/usr/bin/env python3
-export PATH=$PATH:/home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin
+export PATH=$PATH:/PATH/TO/bin/EDTA
 module load bedops/2.4.39
 module load samtools
 
 # 1) Extract individual chromosomes and names file
 #This loop creates fasta files for individual chromosomes in *_genome/chr_by_chr_fasta/, chrnames.txt in *_genome/
-genome_pair="/home/kaedeh/scratch/paired_genome_done_syri/Acer_genome"
+genome_pair="/home/kaedeh/scratch/paired_genome_done_syri/xxxx_genome"
 for genus in $genome_pair
 do
   cat ${genus}/*refgenome_chr.fa | grep ">" | sed s/">"//g > ${genus}/chrnames.txt
@@ -31,15 +31,15 @@ bash ../../while_loop_for_EDTA.sh #Call this from *_genome/chr_by_chr_fasta/ whe
 # 3) Script to run EDTA in Singularity
 for file in `cat chrnames.txt`;
 do
-  singularity exec -B /home -B /project -B /scratch -B /localscratch /home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/EDTA/EDTA-2.0.0.sif \
-perl /home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/EDTA/EDTA_raw.pl \
+  singularity exec -B /home -B /project -B /scratch -B /localscratch /PATH/TO/bin/EDTA/EDTA-2.0.0.sif \
+perl /PATH/TO/bin/EDTA/EDTA_raw.pl \
 --genome chr_by_chr_fasta/${file}.fwd.fa \
 --cds aradu.V14167.gnm1.ann1.cxSM.cds.fna \
 --exclude Arachis_duranensis_chr_renamed_cds.bed \
 --overwrite 1 --sensitive 1 --anno 1 --threads ${SLURM_CPUS_PER_TASK}
 
-  singularity exec -B /home -B /project -B /scratch -B /localscratch /home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/EDTA/EDTA-2.0.0.sif \
-perl /home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/EDTA/EDTA.pl \
+  singularity exec -B /home -B /project -B /scratch -B /localscratch /PATH/TO/bin/EDTA/EDTA-2.0.0.sif \
+perl /PATH/TO/EDTA/EDTA.pl \
 --genome chr_by_chr_fasta/${file}.fwd.fa \
 --cds aradu.V14167.gnm1.ann1.cxSM.cds.fna \
 --exclude Arachis_duranensis_chr_renamed_cds.bed \
@@ -58,4 +58,4 @@ done
 #Concatenate all chromosomes TE results
 cat completed_EDTA/*.mod.EDTA.TEanno.bed >> All_chr_EDTA.TEanno.bed
 #If it requires renaming of chromosomes
-cat All_chr_EDTA.TEanno.bed | perl /home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/replace_contig_names.pl *_chrnames_to_replace.txt > All_chr_names_fixed_EDTA.TEanno.bed
+cat All_chr_EDTA.TEanno.bed | perl /PATH/TO/replace_contig_names.pl *_chrnames_to_replace.txt > All_chr_names_fixed_EDTA.TEanno.bed
