7cf045eecf434864d9ce7c2680c8bc1cd5ac47a6
kaede0e <91504464+kaede0e@users.noreply.github.com>
2022-03-25 08:31:02 -0700

    Update run_minimap2.sh

--- 02_genome_alignment_SV_detection/run_minimap2.sh
+++ 02_genome_alignment_SV_detection/run_minimap2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
-#SBATCH --account=def-gowens
+#SBATCH --account=YOURACCOUNT
 #SBATCH --ntasks=6
 #SBATCH --mem-per-cpu=6G
 
-export PATH=$PATH:/home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin2/anchorwave/minimap2/
+export PATH=$PATH:/PATH/TO/anchorwave/minimap2/
 
 minimap2 -ax asm5 --eqx refgenome.fa qrygenome.fa > minimap2_whole_genome_alignment.sam
