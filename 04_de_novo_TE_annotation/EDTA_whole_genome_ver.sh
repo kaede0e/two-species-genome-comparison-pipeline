c6f0924cb3d19410060d65401ee6b501fef8f402
kaede0e <91504464+kaede0e@users.noreply.github.com>
2022-03-25 08:39:08 -0700

    Update EDTA_whole_genome_ver.sh

--- 04_de_novo_TE_annotation/EDTA_whole_genome_ver.sh
+++ 04_de_novo_TE_annotation/EDTA_whole_genome_ver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=5-00:00:00
-#SBATCH --account=def-gowens
+#SBATCH --account=YOURACCOUNT
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=2000M
@@ -10,10 +10,10 @@ for FOLDER in $(find . -maxdepth 1 -type d | tail -n +2); do
   echo -ne "$FOLDER:\t"
   find $FOLDER -type f | wc -l
 done #This prints how many files there are in your current directory, by subdirectory.
-diskusage_report #check how much more your account can support.
+diskusage_report #check how much more your account can support. EDTA creates hundreds of thousands of intermediate files and can overload your disk. 
 
-singularity exec -B /home -B /project -B /scratch -B /localscratch /home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/EDTA/EDTA-2.0.0.sif \
-perl /home/kaedeh/projects/def-gowens/kaedeh/cranberry_genome/bin/EDTA/EDTA.pl \
+singularity exec -B /home -B /project -B /scratch -B /localscratch /PATH/TO/bin/EDTA/EDTA-2.0.0.sif \
+perl /PATH/TO/bin/EDTA/EDTA.pl \
 --genome refgenome_chr.fa \
 --cds genome_cds.fasta \
 --exclude genome_cds.bed \
