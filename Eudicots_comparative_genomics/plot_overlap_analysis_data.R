1d98edae466d37e67ae7f3bc190279fae1bab070
kaede0e <91504464+kaede0e@users.noreply.github.com>
2022-05-06 15:54:19 -0700

    Create plot_overlap_analysis_data.R

---
+++ Eudicots_comparative_genomics/plot_overlap_analysis_data.R
@@ -0,0 +1,40 @@
+#CDS/TE counts in inversion/syntenic/inversion_breakpoints/random_4k region
+library(ggplot2)
+library(compiler)
+library(tidyverse)
+library(dplyr)
+
+#####Plotting##### 
+#Make sure it's `nCDS(bp)` not single quote!! ''
+Master_bedtools_bpoverlap_merged_CDS <- read_csv("Master_bedtools_bpoverlap_CDS.csv")
+Master_bedtools_bpoverlap_merged_TE <- read_csv("Master_bedtools_bpoverlap_TE.csv")
+Master_bedtools_bpoverlap_breakpoints_merged_cds <- read_csv("Master_bedtools_bpoverlap_breakpoints_merged_cds.csv")
+Master_bedtools_bpoverlap_breakpoints_merged_TE <- read_csv("Master_bedtools_bpoverlap_breakpoints_merged_TE.csv")
+
+CDS_proportion <- Master_bedtools_bpoverlap_merged_CDS %>%
+  mutate(region_length = region_end_1 - region_start_1)%>%
+  group_by(region_start_1, region_end_1, region_length, type, Genus)%>%
+  summarise(
+    total_bpoverlap = sum(`nCDS(bp)`, na.rm = TRUE)
+  )%>%
+  group_by(Genus, type)%>%
+  summarise(
+    proportion_of_CDS_per_region = sum(total_bpoverlap)/sum(region_length)
+  )%>%
+  ggplot()+
+  geom_point(aes(x = type, y = proportion_of_CDS_per_region, colour = type))+
+  geom_line(aes(x = type, y = proportion_of_CDS_per_region, group = Genus))
+
+TE_proportion <- Master_bedtools_bpoverlap_merged_TE %>%
+  mutate(region_length = region_end_1 - region_start_1)%>%
+  group_by(region_start_1, region_end_1, region_length, type, Genus)%>%
+  summarise(
+    total_bpoverlap = sum(`nTE(bp)`, na.rm = TRUE)
+  )%>%
+  group_by(Genus, type)%>%
+  summarise(
+    proportion_of_TE_per_region = sum(total_bpoverlap)/sum(region_length)
+  )%>%
+  ggplot()+
+  geom_point(aes(x = type, y = proportion_of_TE_per_region, colour = type))+
+  geom_line(aes(x = type, y = proportion_of_TE_per_region, group = Genus))
