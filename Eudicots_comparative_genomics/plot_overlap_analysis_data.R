814e8440c632b3a9289ab2ea266f0b904b2d00c8
kaede0e <91504464+kaede0e@users.noreply.github.com>
2022-05-11 13:19:24 -0700

    Update plot_overlap_analysis_data.R

--- Eudicots_comparative_genomics/plot_overlap_analysis_data.R
+++ Eudicots_comparative_genomics/plot_overlap_analysis_data.R
@@ -36,3 +36,33 @@ TE_proportion <- Master_bedtools_bpoverlap_merged_TE %>%
   ggplot()+
   geom_point(aes(x = type, y = proportion_of_TE_per_region, colour = type))+
   geom_line(aes(x = type, y = proportion_of_TE_per_region, group = Genus))
+
+CDS_proportion2 <- Master_bedtools_bpoverlap_breakpoints_merged_cds %>%
+  group_by(Genus, type)%>%
+  summarise(proportion_of_CDS_per_region = sum(`nCDS(bp)`)/sum(region_length))%>%
+  #filter(proportion_of_CDSs <= 0.3)%>% # Arachis was outlier
+  ggplot()+
+  #geom_point(aes(x = type, y = proportion_of_CDS_per_region, colour = type))+
+  #geom_line(aes(x = type, y = proportion_of_CDS_per_region, group = Genus))
+  geom_boxplot(aes(x=type, y=proportion_of_CDS_per_region, colour = type))
+
+TE_proportion2 <- Master_bedtools_bpoverlap_breakpoints_merged_TE %>%
+  mutate(region_length = region_end_1 - region_start_1)%>%
+  group_by(region_start_1, region_end_1, region_length, type, Genus)%>%
+  summarise(
+    total_bpoverlap = sum(`nTE(bp)`, na.rm = TRUE)
+  )%>%
+  group_by(Genus, type)%>%
+  summarise(
+    proportion_of_TE_per_region = sum(total_bpoverlap)/sum(region_length)
+  )%>%
+  ggplot()+
+  #geom_point(aes(x = type, y = proportion_of_TE_per_region, colour = type))+
+  #geom_line(aes(x = type, y = proportion_of_TE_per_region, group = Genus))
+  geom_boxplot(aes(x=type, y=proportion_of_TE_per_region, colour = type))
+
+# combined summary dataframe: 
+joined_proportion <- left_join(CDS_proportion, TE_proportion)
+joined_proportion2 <- left_join(CDS_proportion2, TE_proportion2)
+Master_bedtools_bpoverlap_summary <- rbind(joined_proportion, joined_proportion2) #this summary file is found in the Github
+
