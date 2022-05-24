303d7d9d71ac3e451ce3c9af853e79da26a1f965
kaede0e <91504464+kaede0e@users.noreply.github.com>
2022-05-24 14:27:23 -0700

    Update bedtools_for_all.sh

--- 05_CDS_TE_overlap_analysis/bedtools_for_all.sh
+++ 05_CDS_TE_overlap_analysis/bedtools_for_all.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-# bedtools for getting stats around overlapping sequences between two files
+# bedtools for getting stats around overlapping sequences between two files (bed)
 module load bedtools/2.30.0 #current version (Feb 16, 2022)
 ##For each feature defined in "A", how many features in "B" are overlapping? -c
 ##For each feature defined in "A", how many basepairs are overlapping? -wo
-#For the basepair count, we should merge the CDS first so that there is no duplicates/isoforms that overrepresent those CDS.
+
+#WARNING: 
+#Merge CDS or TE features before doing bedtools intersect so that there is no duplicates/isoforms that could lead to overrepresentation.
 
 #1) Comparison between inverted region vs. syntenic region
 ## Make bed file out of inversion/syntenic table for each genus
 cut -f 1-3 invOut_table.txt | uniq | sed '1d' > inversion_regions.bed
 cut -f 1-3 synOut_table.txt | uniq | sed '1d' > syntenic_regions.bed
-cut -f 1-3 *_refgenome_chr.fa.mod.EDTA.TEanno.bed > genome_{}_TE.bed
+cut -f 1-3 *cds.bed
+cut -f 1-3 *_refgenome_chr.fa.mod.EDTA.TEanno.bed > genome_${genus}_TE.bed
 
-for genus in `cat genome_pair.txt`; #genome_pair.txt lists my parent directory (ie. Citrus_genome, etc.) in rows
+for genus in `cat genome_pair.txt`; #genome_pair.txt lists my parent directory (ie. Acer, Actinidia, Acer, etc.) in rows
 do
   sort -k1,1 -k2,2n ${genus}_genome/*_reduced_cds.bed > ${genus}_genome/sorted_genome_cds.bed
   bedtools merge -i ${genus}_genome/sorted_genome_cds.bed > ${genus}_genome/merged_cds.bed
