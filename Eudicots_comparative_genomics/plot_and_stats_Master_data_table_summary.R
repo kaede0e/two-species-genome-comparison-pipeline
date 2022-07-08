4ebc14fd3fb9c39b966d898201f89e1943537cc3
kaede0e <91504464+kaede0e@users.noreply.github.com>
2022-07-08 13:59:04 -0700

    Update plot_and_stats_Master_data_table_summary.R

--- Eudicots_comparative_genomics/plot_and_stats_Master_data_table_summary.R
+++ Eudicots_comparative_genomics/plot_and_stats_Master_data_table_summary.R
@@ -29,19 +29,46 @@ Master_data_table_summary %>%
 
 #Supplementary Figures 
 #S1a) Distribution of syntenic regions identified by SyRI with respect to its sequence identity 
+syn_plot %>%
+  ggplot() +
+  geom_histogram(aes(x=region_percent_ID), binwidth = 0.002) +
+  facet_wrap("Genus", nrow = 5) +
+  geom_vline(aes(xintercept=syn_divergence))
 
 #S1b) Distribution of inverted regions identified by SyRI with respect to its sequence identity 
+syn_inv_plot %>%
+  filter(type == "inv") %>%
+  ggplot() +
+  geom_histogram(aes(x=region_percent_ID), binwidth = 0.002) +
+  facet_wrap("Genus", nrow = 5) 
 
-#S2a) Number of inversions plotted against sequence divergence grouped by self-compatibility category
+#S2a) Number of inversions plotted against sequence divergence grouped by hybridization category
+Master_data_table_summary %>%
+  filter(ref_or_qry == "ref") %>%
+  ggplot()+
+  geom_point(aes(x=percent_divergence, y=total_inv_regions_number, colour = Evidence_of_hybridization))+ 
+  geom_smooth(aes(x=percent_divergence, y=total_inv_regions_number, colour = Evidence_of_hybridization), method = "lm")+ 
+  ylab("Number of inversions ≥1000bp") +xlab("Sequence divergence (%)")
 
-#S2b) Total number of inversions grouped by self-compatibility category
-
-#S2c) Number of inversions plotted against sequence divergence grouped by hybridization category
-
-#S2d) Total number of inversions grouped by hybridization category
+#S2b) Total number of inversions grouped by hybridization category
 Master_data_table_summary %>%
   filter(ref_or_qry == "ref") %>%
   filter(!is.na(Evidence_of_hybridization)) %>%
   ggplot()+
   geom_boxplot(aes(x=Evidence_of_hybridization, y=total_inv_regions_number))+ ylab("Number of inversions ≥1000bp")+ xlab("Evidence of hybridization")
+
+#S2c) Number of inversions plotted against sequence divergence grouped by self-compatibility category
+Master_data_table_summary %>%
+  filter(ref_or_qry == "ref") %>%
+  ggplot()+
+  geom_point(aes(x=percent_divergence, y=total_inv_regions_number, colour = `Self-compatibility`))+ 
+  geom_smooth(aes(x=percent_divergence, y=total_inv_regions_number, colour = `Self-compatibility`), method = "lm")+ 
+  ylab("Number of inversions ≥1000bp") +xlab("Sequence divergence (%)")
+
+#S2d) Total number of inversions grouped by self-compatibility category
+Master_data_table_summary %>%
+  filter(ref_or_qry == "ref") %>%
+  filter(!is.na(`Self-compatibility`)) %>%
+  ggplot()+
+  geom_boxplot(aes(x=`Self-compatibility`, y=total_inv_regions_number))+ ylab("Number of inversions ≥1000bp")
 
