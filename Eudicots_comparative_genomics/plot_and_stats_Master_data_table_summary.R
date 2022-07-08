9681fc21b3a2a5414465464f4385d821999e6f77
kaede0e <91504464+kaede0e@users.noreply.github.com>
2022-07-08 12:35:56 -0700

    Update plot_and_stats_Master_data_table_summary.R

--- Eudicots_comparative_genomics/plot_and_stats_Master_data_table_summary.R
+++ Eudicots_comparative_genomics/plot_and_stats_Master_data_table_summary.R
@@ -1,7 +1,8 @@
 #what parameter(s) affects the number of inversions? 
 library(tidyverse)
 
-#####Plotting#####
+#####Plotting and statistical analysis#####
+Master_data_table_summary <- read_csv("Master_data_table_summary_all_genomes_updated.csv")
 Master_data_table_summary %>%
   left_join(., biological_info)%>%
   #filter(Evidence_of_hybridization == "weak")%>%
@@ -25,3 +26,22 @@ Master_data_table_summary %>%
         Genome_length, 
     data = .) %>%
   summary()
+
+#Supplementary Figures 
+#S1a) Distribution of syntenic regions identified by SyRI with respect to its sequence identity 
+
+#S1b) Distribution of inverted regions identified by SyRI with respect to its sequence identity 
+
+#S2a) Number of inversions plotted against sequence divergence grouped by self-compatibility category
+
+#S2b) Total number of inversions grouped by self-compatibility category
+
+#S2c) Number of inversions plotted against sequence divergence grouped by hybridization category
+
+#S2d) Total number of inversions grouped by hybridization category
+Master_data_table_summary %>%
+  filter(ref_or_qry == "ref") %>%
+  filter(!is.na(Evidence_of_hybridization)) %>%
+  ggplot()+
+  geom_boxplot(aes(x=Evidence_of_hybridization, y=total_inv_regions_number))+ ylab("Number of inversions ≥1000bp")+ xlab("Evidence of hybridization")
+
