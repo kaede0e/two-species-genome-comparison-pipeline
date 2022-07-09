44fdd60e97908801343046415d1c4807f056e18c
kaede0e <91504464+kaede0e@users.noreply.github.com>
2022-07-08 17:13:36 -0700

    Update plot_and_stats_Master_data_table_summary.R

--- Eudicots_comparative_genomics/plot_and_stats_Master_data_table_summary.R
+++ Eudicots_comparative_genomics/plot_and_stats_Master_data_table_summary.R
@@ -2,30 +2,6 @@
 library(tidyverse)
 
 #####Plotting and statistical analysis#####
-Master_data_table_summary <- read_csv("Master_data_table_summary_all_genomes_updated.csv")
-Master_data_table_summary %>%
-  left_join(., biological_info)%>%
-  #filter(Evidence_of_hybridization == "weak")%>%
-  mutate(TE_proportion = total_TE_length/Genome_length) %>%
-  ggplot(.,aes(x=percent_divergence, y=number_of_inversion_regions)) +
-  geom_point() + 
-  geom_smooth(method="lm")
-
-Master_data_table_summary %>%
-  left_join(., biological_info)%>%
-  #filter(`Self-compatibility`=="mixed")%>%
-  aov(number_of_inversion_regions ~ percent_divergence +
-        `Self-compatibility`, 
-      data= .) %>%
-  summary()
-
-Master_data_table_summary %>%
-  mutate(TE_proportion = total_TE_length/Genome_length) %>%
-  aov(inv_region_length ~ syn_divergence + 
-        TE_proportion +
-        Genome_length, 
-    data = .) %>%
-  summary()
 
 #Figures
 #1) Density of sequence identity score inside inverted region (blue) vs. syntenic region (red) for 32 species pair. 
